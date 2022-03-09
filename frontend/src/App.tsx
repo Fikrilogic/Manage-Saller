b0abc3358f773f54d33db7d280b887cc2a224a12
Fikrilogic <fikrisandi3074@gmail.com>
2022-03-09 21:34:18 +0700

    delete route login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ import AddSellerDashboard from "./pages/dashboard/Seller/add-sellers.dashboard";
 import EditSellerDashboard from "./pages/dashboard/Seller/edit-sellers";
 import EditData from "./pages/dashboard/Seller/edit";
 import Dashboard from "./pages/dashboard/main-dashboard";
-import Login from "./pages/admin-login";
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
         <Routes>
           /
           <Route path="/" element={<Navigate to={"dashboard"} />} />
-          <Route path="login" element={<Login />} />
           <Route path="dashboard" element={<Dashboard />}>
             <Route path="sellers" element={<LookSeller />} />
             <Route path="sellers/add" element={<AddSellerDashboard />} />
